4ff6128790d5f31aace893048033b40158eb7024
Alex Kazaryan <alexkazaryan@outlook.com>
2020-05-03 15:03:30 +0300

    Add game section and trial flip-cards

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btnPlay = document.getElementById('btn-play');
 const header = document.getElementById('header');
 const headingPlayMain = document.getElementById('heading-play--main');
 const headingPlaySub = document.getElementById('heading-play--sub');
+const game = document.getElementById('game');
 
 btnPlay.addEventListener('click', () => {
 	btnPlay.classList.remove('btn--moveIn');
@@ -9,4 +10,19 @@ btnPlay.addEventListener('click', () => {
 	headingPlayMain.classList.add('heading-play--main--moveOut');
 	headingPlaySub.classList.add('heading-play--sub--moveOut');
 	header.classList.add('header--moveOutBorder');
+	setTimeout(showGame, 1000);
+});
+
+function showGame() {
+	header.classList.add('hide');
+	game.classList.remove('hide');
+	game.classList.add('show');
+}
+
+var cards = document.querySelectorAll('.card');
+
+cards.forEach(function (item, idx) {
+	item.addEventListener('click', function () {
+		item.classList.toggle('is-flipped');
+	});
 });
